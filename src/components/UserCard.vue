1c0faf57d261008b4f86f1fc3582b3a714892065
David M.Chen <chen7david@me.com>
2020-11-25 14:25:30 +0800

    update

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -2,46 +2,41 @@
   <v-card width="400" height="100" color="#263238" class="white--text" tile>
     <v-app-bar flat color="rgba(0, 0, 0, 0)" class="ma-0">
             <v-toolbar-title class="title white--text text-capitalize">
-              {{device.browser.name}}
+              {{item.email}}
             </v-toolbar-title>
 
             <v-spacer></v-spacer>
 
-            <v-btn color="white" icon v-if="isActive">
-              <v-icon @click="revoke" small>mdi-close</v-icon>
+            <UserUpdateForm class="ma-0" :item="item"/>
+            <v-btn color="white" icon>
+              <v-icon small>mdi-close</v-icon>
             </v-btn>
     </v-app-bar>
     <v-card-subtitle class="ma-0">
-      {{device.platform}} {{device.os.name}} {{device.os.version.info}} | {{item.call_count}} | {{fromNow}}
+      {{item.phone}} | {{age}}
     </v-card-subtitle>
   </v-card>
 </template>
 
 <script>
-import ua from 'ua_parser'
 import moment from 'moment'
-
+import UserUpdateForm from './UserUpdateForm'
 export default {
     name:'UserCard',
+    components: {
+       UserUpdateForm
+    },
     props: {
         item: null
     },
     data: () => ({
-        tokens: [],
     }),
     computed: {
-      device(){
-        return ua.userAgent(this.item.useragent)
-      },
       fromNow(){
         return moment(this.item.updated_at).fromNow()
       },
-      bgColor(){
-          if(this.item.revoked) return '#E65100'
-          return this.item.active ? '#303F9F' : '#263238'
-      },
-      isActive(){
-        return this.item.active && !this.item.revoked
+      age(){
+        return this.item.birthDate ? moment().diff(this.item.birthDate, 'years') : 0
       }
     },
     methods: {
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -4,33 +4,8 @@
             <v-col cols="12">
                 <v-row fill-height justify="space-around">
                     <v-col v-for="user of users" :key="user.id">
-                        <UserCard :user="user"/>
+                        <UserCard :item="user"/>
                     </v-col>
-                    <!-- <v-data-table
-                        :headers="headers"
-                        :items="users"
-                        class="elevation-1"
-                        
-                        item-key="name"
-                    >
-                        <template v-slot:items="props">
-                             <tr :active="props.selected" @click="props.selected = !props.selected">
-                            <td>
-                                <v-checkbox
-                                    :input-value="props.selected"
-                                    primary
-                                    hide-details
-                                ></v-checkbox>
-                            </td>
-                                <td>{{ props.item.name }}</td>
-                                <td class="text-xs-right">{{ props.item.calories }}</td>
-                                <td class="text-xs-right">{{ props.item.fat }}</td>
-                                <td class="text-xs-right">{{ props.item.carbs }}</td>
-                                <td class="text-xs-right">{{ props.item.protein }}</td>
-                                <td class="text-xs-right">{{ props.item.iron }}</td>
-                             </tr>
-                        </template>
-                    </v-data-table> -->
                 </v-row> 
             </v-col>
         </v-row>
@@ -46,13 +21,6 @@ export default {
         UserCard
     },
     data: () => ({
-        headers: [
-            { text: 'UserID', value: 'userId' },
-            { text: 'Phone', value: 'phone' },
-            { text: 'Email', value: 'email' },
-            { text: 'First Name', value: 'firstName' },
-            { text: 'LastName', value: 'lastName' },
-        ],
         users: [],
     }),
     computed: {
